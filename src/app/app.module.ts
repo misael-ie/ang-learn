8b1e88317ea6a5f3f9bf0b63de329a3b46055b05
misael-ie <misaspy@gmail.com>
2021-12-20 20:29:12 -0300

    split header and home into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import locatePt from '@angular/common/locales/pt'
 registerLocaleData(locatePt, 'pt')
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'zero-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
