cfb07427bb707f7547ce281ff47d475b1dd54f3c
misael-ie <misaspy@gmail.com>
2021-12-21 08:43:40 -0300

    sub-components com rotas específicas: 'Criando componentes de detalhes de restaurantes'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,10 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { RestaurantsService } from './restaurants/restaurants.service';
 import { HttpClientModule } from '@angular/common/http';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +28,11 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    ShoppingCartComponent,
+    MenuComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
