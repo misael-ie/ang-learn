94e9a0e1f85d1778764df6bdb6d594c2328ff67c
misael-ie <misaspy@gmail.com>
2021-12-28 07:13:57 -0300

    89. Animação nos Itens do Menu

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,14 +1,30 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 import { MenuItem } from './menu-item.model';
 
 @Component({
   selector: 'restaurant-detail-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({
+        opacity: 1
+      })),
+      transition("void => ready", [
+        style({
+          opacity: 0,
+          transform: 'translate(0px, 15px)'
+        }),
+        animate('500ms 0s ease-in')
+      ])
+    ])
+  ]  
 })
 export class MenuItemComponent implements OnInit {
 
   @Input() menuItem!: MenuItem
   @Output() add = new EventEmitter()
+  menuItemState = "ready"
 
   constructor() { }
 
