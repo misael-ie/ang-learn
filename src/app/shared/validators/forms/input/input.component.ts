05dbac86fcd76cb36d3a7af7eb308fe99d65e7bd
misael-ie <misaspy@gmail.com>
2021-12-31 10:08:57 -0300

    (minor) add showTip flag to input form component

--- src/app/shared/validators/forms/input/input.component.ts
+++ src/app/shared/validators/forms/input/input.component.ts
@@ -18,6 +18,7 @@ export class InputComponent implements OnInit, AfterContentInit {
   @Input() label:string = ""
   @Input() successMessage:string = ""
   @Input() errorMessage:string = ""
+  @Input() showTip:boolean = true
   input:any
 
   @ContentChild(NgModel) model!: NgModel
